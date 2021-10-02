47704ef710318941f80e210a1efa41d4f67c42af
Lee2532 <seungoh0423@gmail.com>
2021-10-02 21:46:38 +0900

    nestjs최초 커밋

---
+++ src/cats/cats.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { Cat, CatDocument } from './schemas/cat.schema';
+
+@Injectable()
+export class CatsService {
+  constructor(
+    @InjectModel(Cat.name) private readonly catModel: Model<CatDocument>,
+  ) {}
+
+  async create(createCatDto: CreateCatDto): Promise<Cat> {
+    const createdCat = new this.catModel(createCatDto);
+    return createdCat.save();
+  }
+
+  async findAll(): Promise<Cat[]> {
+    return this.catModel.find().exec();
+  }
+
+  async findOne(): Promise<any[]> {
+    return this.catModel.find().exec();
+    // return this.catModel.findOne({name?:string}).exec();
+  }
+
+}
