4324894eaab255a45e67748e432cb0a614bab2f6
Lee2532 <seungoh0423@gmail.com>
2021-10-06 22:19:48 +0900

    Postgresql 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsModule } from './cats/cats.module';
+
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    TypeOrmModule.forRoot(),
     CatsModule,
   ],
 })
