38b54ac787665f508414a61f4276cfd5806ff874
brandon <brandon@marketdesigners.com>
2021-10-21 14:16:58 +0900

    logger 추가 (모건 lib)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { logger } from './common/common.logger';
+// import { logger } from './common/common.logger';
+import morgan from 'morgan'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(logger)
+  let logger = require('morgan');
+
+  app.use(logger('combined'))
+
   await app.listen(3000);
 }
 bootstrap();
