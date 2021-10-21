ef73b26efda11935fe8d75b47919683f6868e3c3
brandon <brandon@marketdesigners.com>
2021-10-21 14:52:26 +0900

    os 확인로직 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+
 @Injectable()
 export class UserService {
   getHello(): string {
-    return 'Hello World!';
+    let os = require('os');
+    let osname:string;
+    if (os.type() == 'Darwin'){
+      osname = 'mac';
+    }
+    else if (os.type() == 'Linux'){
+      osname = 'linux';      
+    }
+    else if (os.type() == 'Windows_NT'){
+      osname = 'Windows';      
+    }
+    else{
+      osname = undefined
+    }
+    
+    return `Hello ${osname} World!`;
   }
 }
