95959b96bee3d8c22ea67c9690bd1d112ad1e2b9
brandon <brandon@marketdesigners.com>
2021-10-21 14:30:40 +0900

    user 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsModule } from './cats/cats.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/test'),
     TypeOrmModule.forRoot(),
     CatsModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,14 @@
-import { Controller } from '@nestjs/common';
-
+import { HttpCode, Header, Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Get()
+    async hello(): Promise<string>{
+        return this.userService.getHello();
+
+    }
+
+}
