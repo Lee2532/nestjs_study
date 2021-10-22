bd612db279b2f0f8f5bdfd1c763cf708cc5c9889
brandon <brandon@marketdesigners.com>
2021-10-22 15:07:00 +0900

    add cors origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { AppModule } from './app.module';
 import morgan from 'morgan'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const appOptions = {
+    cors:{
+      origin : [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+      ]
+    }
+  }
+
+  const app = await NestFactory.create(AppModule, appOptions);
   let logger = require('morgan');
 
   app.use(logger('combined'))
